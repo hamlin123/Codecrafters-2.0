158390aa0ec8cce0e4eebb46f05b983a17b2a58d
hamlin123 <mariahamlin2006@gmail.com>
2024-10-09 10:51:51 +0000

    condition

---
+++ main.py
@@ -0,0 +1,16 @@
+print("hello world")
+Num_01: int =10
+Floating: float  =10.5
+String: str ="hello"
+boolean: bool=True
+name:str=input("enter ur name:")
+print(" the name is", name)
+number_1: int=float(input("enter a number 1 :"))
+number_2: int=int(input("enter a number 2:"))
+if number_1==number_2:
+    print("both are same")
+elif number_1>number_2:
+    print("both are different")
+
+
+
